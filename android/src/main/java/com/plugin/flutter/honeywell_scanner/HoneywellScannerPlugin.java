ef87d9e36b3da9852bd7bc58d62c773b43280aa8
Ørjan <orjantrulsensvendsen@gmail.com>
2020-06-02 12:38:13 +0200

    test

--- android/src/main/java/com/plugin/flutter/honeywell_scanner/HoneywellScannerPlugin.java
+++ android/src/main/java/com/plugin/flutter/honeywell_scanner/HoneywellScannerPlugin.java
@@ -112,6 +112,7 @@ public class HoneywellScannerPlugin implements MethodCallHandler, ScannerCallBac
      * @param code Encapsulates the result of decoding a barcode within an image
      */
     @Override
+    @optionalTypeArgs
     public void onDecoded(final String code, final String CodeId)
     {
         handler.post(() -> channel.invokeMethod(_ON_DECODED, [code, CodeId]));
